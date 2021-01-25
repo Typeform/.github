05ce5844c45971fed67fabcfdf56e1da179064e0
Kao Félix <kao.felix@typeform.com>
2021-01-25 18:52:11 +0100

    feat: use official gitleaks image

--- shared-actions/secrets-scan/secrets-scan.sh
+++ shared-actions/secrets-scan/secrets-scan.sh
@@ -17,20 +17,15 @@ if [ ! $exit_code -eq 0 ]; then
     exit 1
 fi
 
-# If this is being ran as a Github ACtion use $GITHUB_WORKSPACE
-if [ -z "$GITHUB_WORKSPACE" ]; then
-    repo_dir=$PWD
-else
-    repo_dir=$GITHUB_WORKSPACE
-fi
-
+repo_dir=$GITHUB_WORKSPACE
 repo_name="$(basename "$repo_dir")"
 
 # Generate gitleaks configuration
 local_config=".gitleaks.toml"
 final_config="/tmp/gitleaks_config.toml"
 gitleaks_config_container="${DOCKER_REGISTRY}/typeform/gitleaks-config"
-gitleaks_container="${DOCKER_REGISTRY}/typeform/gitleaks"
+gitleaks_container="zricethezav/gitleaks"
+gitleaks_version="v7.2.0"
 
 # Generate the final gitleaks config file. If the repo has a local config, merge both
 if [ -f ./"$local_config" ]; then
@@ -41,8 +36,21 @@ else
     python gitleaks_config_generator.py > $final_config
 fi
 
+if [ -z "${GITHUB_BASE_REF}" ]; then
+    # We're on master
+    commit_opts="--commit=${GITHUB_SHA}"
+else
+    # We're on a PR
+    git --git-dir="$GITHUB_WORKSPACE/.git" log \
+        --left-right --cherry-pick --pretty=format:"%H" \
+        remotes/origin/$GITHUB_BASE_REF... > commit_list.txt
+
+    commit_opts="--commit-file=commit_list.txt"
+fi
+
 # Run gitleaks with the generated config
 docker container run --rm --name=gitleaks \
     -v $final_config:$final_config \
     -v $repo_dir:/tmp/$repo_name \
-    $gitleaks_container --config=$final_config --repo=/tmp/$repo_name --verbose --pretty
+    $gitleaks_container:$gitleaks_version --config=$final_config --repo=/tmp/$repo_name --verbose --pretty \
+    $commit_opts
