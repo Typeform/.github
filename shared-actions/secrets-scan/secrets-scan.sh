f799b06a5cc2a63cd4979bd3cf7be021d126dba8
Kao Félix <kao.felix@typeform.com>
2021-01-26 12:34:55 +0100

    [SP-894] Create a Shared GH Action for Running Gitleaks (#3)
    feat(secret-detection): [SP-894] shared action for running gitleaks
    
    Co-authored-by: David Salvador <62070820+davidsalvador-tf@users.noreply.github.com>

---
+++ shared-actions/secrets-scan/secrets-scan.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Check if docker is installed
+if ! command -v "docker" &> /dev/null
+then
+    echo "Unable to find docker. Is it installed and added to your \$PATH?"
+    exit 1
+fi
+
+# Check if user is logged in to quay.io
+DOCKER_REGISTRY=quay.io
+docker pull ${DOCKER_REGISTRY}/typeform/gitleaks-config
+exit_code=$?
+
+if [ ! $exit_code -eq 0 ]; then
+    echo "Unable to pull gitleaks container image. Are you logged in ${DOCKER_REGISTRY}?"
+    exit 1
+fi
+
+repo_dir=$GITHUB_WORKSPACE
+repo_name="$(basename "$repo_dir")"
+
+# Generate gitleaks configuration
+local_config=".gitleaks.toml"
+final_config="/tmp/gitleaks_config.toml"
+commits_file="/tmp/commit_list.txt"
+gitleaks_config_container="${DOCKER_REGISTRY}/typeform/gitleaks-config"
+gitleaks_container="zricethezav/gitleaks"
+gitleaks_version="v7.2.0"
+
+# Generate the final gitleaks config file. If the repo has a local config, merge both
+if [ -f ./"$local_config" ]; then
+    docker container run --rm -v $repo_dir/$local_config:/app/$local_config \
+    $gitleaks_config_container python gitleaks_config_generator.py > $final_config
+else
+    docker container run --rm $gitleaks_config_container \
+    python gitleaks_config_generator.py > $final_config
+fi
+
+if [ -z "${GITHUB_BASE_REF}" ]; then
+    # push event
+    commit_opts="--commit=${GITHUB_SHA}"
+else
+    # pull_request event
+    git --git-dir="$GITHUB_WORKSPACE/.git" log \
+        --left-right --cherry-pick --pretty=format:"%H" \
+        remotes/origin/$GITHUB_BASE_REF... > $commits_file
+
+    commit_opts="--commits-file=${commits_file}"
+fi
+
+# Run gitleaks with the generated config
+docker container run --rm --name=gitleaks \
+    -v $final_config:$final_config \
+    -v $commits_file:$commits_file \
+    -v $repo_dir:/tmp/$repo_name \
+    $gitleaks_container:$gitleaks_version --config-path=$final_config --path=/tmp/$repo_name --verbose \
+    $commit_opts
