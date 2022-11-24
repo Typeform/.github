918dd7bcfc335790df898f31d5ddd34b9f33e155
Lucho Palanca <luis@typeform.com>
2022-11-24 11:15:42 +0100

    feat(NOJIRA-1234): WIP entrypoint 5

--- shared-actions/godoc-to-html/entrypoint.sh
+++ shared-actions/godoc-to-html/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -l
 
-ls -la /github/workspace/
-cd /github/workspace/
+cd /github/workspace/go
+ls -la .
 godoc -v -http=:6060 &
 
-echo "GODOCS FOR $1"
+REPOSITORY_NAME=blocks/go
+
+sudo apt-get install -y wget
+
+wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/Typeform/blocks/go,/src/github.com/Typeform/blocks/go" --exclude-directories="*" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/Typeform/blocks/go
+
+mkdir -p /github/workspace/build-docs/blocks/godocs
+mv godocs /github/workspace/build-docs/blocks
