1e41ea9a0f91ca2890e1df803b2b8d4da5443253
Lucho Palanca <luis@typeform.com>
2022-11-24 11:56:36 +0100

    feat(NOJIRA-1234): WIP entrypoint 7

--- shared-actions/godoc-to-html/entrypoint.sh
+++ shared-actions/godoc-to-html/entrypoint.sh
@@ -2,11 +2,13 @@
 
 cd /github/workspace/go
 ls -la .
+
+mkdir -p "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
+cp -r * "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
+
 godoc -v -http=:6060 &
 
 REPOSITORY_NAME=blocks/go
-
-apt-get install -y wget
 
 wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/Typeform/blocks/go,/src/github.com/Typeform/blocks/go" --exclude-directories="*" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/Typeform/blocks/go
 
