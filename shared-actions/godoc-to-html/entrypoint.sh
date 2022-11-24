053b37afb5aa84d8873685dc1504459b64ef0910
Lucho Palanca <luis@typeform.com>
2022-11-24 15:05:37 +0100

    feat(NOJIRA-1234): WIP entrypoint 8

--- shared-actions/godoc-to-html/entrypoint.sh
+++ shared-actions/godoc-to-html/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -l
 
+REPOSITORY_NAME=blocks/go
+
 cd /github/workspace/go
 ls -la .
 
 mkdir -p "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
 cp -r * "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
 
-godoc -v -http=:6060 &
+ls -la $GOPATH/src/github.com/$GITHUB_REPOSITORY
 
-REPOSITORY_NAME=blocks/go
+godoc -v -http=:6060 &
 
 wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/Typeform/blocks/go,/src/github.com/Typeform/blocks/go" --exclude-directories="*" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/Typeform/blocks/go
 
