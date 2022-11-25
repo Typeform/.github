c8d14456d1b6acd294431be32daed5867fd82e21
Lucho Palanca <luis@typeform.com>
2022-11-25 12:29:46 +0100

    feat(NOJIRA-1234): WIP entrypoint 18

--- shared-actions/godoc-to-html/entrypoint.sh
+++ shared-actions/godoc-to-html/entrypoint.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-REPOSITORY_NAME=blocks/go
-
-cd /github/workspace/go
+cd "$(dirname "$(find . -name 'go.mod' | head -n 1)")" || exit 1
 
 mkdir -p "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
 cp -r * "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
 
-export GO111MODULE=on
-export GOFLAGS="-mod=vendor"
-
 go mod vendor
 godoc -v -http=:6060 &
 
-wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/Typeform/$GITHUB_REPOSITORY,/src/github.com/Typeform/$GITHUB_REPOSITORY" --exclude-directories="*" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/Typeform/$GITHUB_REPOSITORY
+wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/$GITHUB_REPOSITORY,/src/github.com/$GITHUB_REPOSITORY" --exclude-directories="*" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/$GITHUB_REPOSITORY
