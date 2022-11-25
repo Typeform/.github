6773b7fe80b998d1b9b909a395b96227d87d962d
Lucho Palanca <luis@typeform.com>
2022-11-25 14:33:10 +0100

    feat(NOJIRA-1234): WIP entrypoint 20

--- shared-actions/godoc-to-html/entrypoint.sh
+++ shared-actions/godoc-to-html/entrypoint.sh
@@ -2,7 +2,6 @@
 
 PATH_GOMOD="$(dirname "$(find . -name 'go.mod' | head -n 1)")" || exit 1
 cd $PATH_GOMOD
-echo $PATH_GOMOD
 
 mkdir -p "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
 cp -r * "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
@@ -10,4 +9,10 @@ cp -r * "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
 go mod vendor
 godoc -v -http=:6060 &
 
-wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/$GITHUB_REPOSITORY,/src/github.com/$GITHUB_REPOSITORY" --exclude-directories="*" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/$GITHUB_REPOSITORY
+REPO=$GITHUB_REPOSITORY
+if [ $PATH_GOMOD != "." ]
+then
+  REPO = "${GITHUB_REPOSITORY}/{$PATH_GOMOD}"
+fi
+
+wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/$REPO,/src/github.com/$REPO" --exclude-directories="*" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/$REPO
