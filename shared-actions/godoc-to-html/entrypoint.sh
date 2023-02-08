6e4ba0a031bfb1ed321fdf5ce432a46b4c2745c0
Lucho Palanca <luis@typeform.com>
2023-02-08 15:42:50 +0100

    Trigger CI 10

--- shared-actions/godoc-to-html/entrypoint.sh
+++ shared-actions/godoc-to-html/entrypoint.sh
@@ -17,7 +17,7 @@ fi
 
 echo $REPO
 
-wget -m -r -N -E -p -k -nd -q --include-directories="/lib,/pkg/github.com/$REPO,/src/github.com/$REPO" --exclude-directories="/pkg/github.com/$REPO/vendor,/src/github.com/$REPO/vendor" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/$REPO
+wget -p -r -l0 -E -k -nH -q --include-directories="/lib,/pkg/github.com/$REPO,/src/github.com/$REPO" --exclude-directories="/pkg/github.com/$REPO/vendor,/src/github.com/$REPO/vendor" --no-host-directories --directory-prefix=godocs http://localhost:6060/pkg/github.com/$REPO
 
 chmod -R 777 godocs
 find ./godocs -type f -iname "*[?]*" -delete
